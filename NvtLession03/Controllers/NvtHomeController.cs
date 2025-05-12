48f66b09d7428ac14547a14a2381bdb84a319b10
NguyenVanThinhK23CNT1 <thinhvannguyen113@gmail.com>
2025-05-12 10:00:07 +0700

    up file

---
+++ NvtLession03/Controllers/NvtHomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NvtLession03.Models;
+
+namespace NvtLession03.Controllers
+{
+    public class NvtHomeController : Controller
+    {
+        private readonly ILogger<NvtHomeController> _logger;
+
+        public NvtHomeController(ILogger<NvtHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult NvtIndex()
+        {
+            return View();
+        }
+
+        public IActionResult NvtAbout()
+        {
+            ViewBag.About = "Văn Thịnh";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult NvtProfile()
+        {
+            return View();
+        }
+
+        public IActionResult NvtRazorCode()
+        {
+            return View();
+        }
+    }
+}
